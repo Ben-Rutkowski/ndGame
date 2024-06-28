fd0879cddcf72dd88980aa586c5b535bc442b561
Ben-Rutkowski <70175023+Ben-Rutkowski@users.noreply.github.com>
2024-06-28 02:17:03 -0700

    added log macros for tests

--- interface/test_success.h
+++ interface/test_success.h
@@ -3,6 +3,7 @@
 
 #define INT_SetAppDelegate  ":INT_SetAppDelegate:"
 #define APP_AppFinishLaunch ":APP_AppFinishLaunch:"
+#define MOD_EventManagerNullCallback ":EventManagerNullCallback:"
 
 #define UNIT_Success ":UNIT_Success:"
 #define UNIT_Failure ":UNIT_Failure:"
